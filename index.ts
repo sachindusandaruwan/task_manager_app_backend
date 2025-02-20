bea15476dd4ab9f06e7c2a34bc9a2105585aedd4
sachindusandaruwan <sadaruwansachindu@gmail.com>
2025-02-20 23:50:10 +0530

    remove prisma & add mongoDb configs

--- index.ts
+++ index.ts
@@ -1,7 +1,11 @@
+import dotenv from 'dotenv';
 import express from 'express';
+import connectDB from './config/db';
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+connectDB();
 
 app.use(express.json());
 
